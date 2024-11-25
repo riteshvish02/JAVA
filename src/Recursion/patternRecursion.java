61d33ed2a5cc8d829cd42425ec64bc16cdab05c2
riteshvish02 <rv504263@gmail.com>
2024-11-25 14:31:16 +0530

    added binary search

---
+++ src/Recursion/patternRecursion.java
@@ -0,0 +1,35 @@
+package Recursion;
+
+public class patternRecursion {
+//    public static void f(int row,int col,int star){
+//     reverse pattern
+
+//        if(star <= 0) return;
+//        if(col > star){
+//            System.out.println();
+//            f(row+1,1,star-1);
+//        }else{
+//            System.out.print("*");
+//            f(row,col+1,star);
+//        }
+//    }
+//    public static void main(String[] args) {
+//        //reverse pattern
+//        int n = 4;
+//        f(1,1,n);
+//    }
+
+    public static void f(String str,int idx){
+
+        if(idx >= str.length()) return;
+        if(str.charAt(idx) != 'x'){
+            System.out.print(str.charAt(idx));
+        }
+        f(str,idx+1);
+    }
+    public static void main(String[] args) {
+        String str = "xxabcxnm";
+        int idx = 0;
+        f(str,idx);
+    }
+}
--- src/search/binary.java
+++ src/search/binary.java
@@ -1,5 +1,25 @@
 package search;
 
 public class binary {
+    public static int binarySearch(int[] arr,int key){
+        int i = 0;
+        int j = arr.length-1;
+        while(i <= j){
+            int mid = (i+j)/2;
+            if(arr[mid] == key){
+               return  mid;
+            }else if(arr[mid] < key){
+                i = mid+1;
+            }else{
+                j = mid-1;
+            }
+        }
+        return -1;
 
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,3,5,7,8,9,10};
+        int key = 10;
+        System.out.println( binarySearch(arr,key));
+    }
 }
