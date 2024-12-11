ad6f83fe5ab3842771a1caedfbcdfbed186d2caa
riteshvish02 <rv504263@gmail.com>
2024-12-11 14:26:38 +0530

    frog problem

---
+++ src/Recursion/recursion6.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class recursion6 {
+    public static int f(int i,int arr[]){
+        if(i >= arr.length-1) return 0;
+        int mincost = Integer.MAX_VALUE;
+        for(int jump = 1;jump <= 3;jump++){
+            if(i+jump < arr.length){
+                int val = Math.abs(arr[i]-arr[i+jump])+f(i+jump,arr);
+                mincost = Math.min(mincost,val);
+            }
+
+        }
+        return mincost;
+    }
+    public static void main(String[] args) {
+        //frog problem (i+1,i+2,i+3);
+        int arr[] = {10, 30, 40, 50, 20};
+        System.out.println(f(0,arr));
+    }
+
+}
