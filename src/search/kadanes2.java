c841ca399dbe734fa39acf9dcae75980a241367a
riteshvish02 <rv504263@gmail.com>
2024-12-22 22:48:14 +0530

    correct kadane's algo for every-case

--- src/search/kadanes2.java
+++ src/search/kadanes2.java
@@ -2,11 +2,11 @@ package search;
 
 public class kadanes2 {
     public static void main(String[] args) {
-        int arr[] = {-1,-2,-5,-6};
+        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
         int currsum = arr[0];
         int maxsum = arr[0];
         for(int i =1;i < arr.length;i++){
-            currsum = Math.max(currsum,currsum + arr[i]);
+            currsum = Math.max(arr[i],currsum + arr[i]);
             maxsum = Math.max(currsum,maxsum);
 
         }
