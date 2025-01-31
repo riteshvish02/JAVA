f3237038a134d05bb97c96a2660ee276891a684b
riteshvish02 <rv504263@gmail.com>
2025-01-31 18:56:04 +0530

    mergingTwoSortedArray

--- src/sort/day2/insertionSort.java
+++ src/sort/day2/insertionSort.java
@@ -2,15 +2,15 @@ package sort.day2;
 
 public class insertionSort {
     public static void main(String[] args) {
-        int[] arr = {3,4,5,9,1,2,3};
+        int[] arr = {1,2,3};
         insertionSort(arr);
         for(int val : arr){
-            System.out.println(val);
+            System.out.print( val + " ");
         }
     }
     public static void insertionSort(int[] arr){
         int n = arr.length;
-        for(int i = 0; i < n;i++){
+        for(int i = 1; i < n;i++){
             int j = i-1;
             int currentElem = arr[i];
             while(j >= 0 && arr[j] > currentElem){ //keep remember
